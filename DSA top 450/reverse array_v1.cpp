8a06e07369a781b2d475007189fd395cecbdc8b6
MeetSoni <mksoni1627@gmail.com>
2021-05-13 23:37:57 +0530

    revese array

---
+++ DSA top 450/reverse array_v1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+        int n;
+        cin>>n;
+
+        vector<int> v;
+
+        for(int i=0;i<n;i++){
+            int temp;
+            cin>>temp;
+            v.push_back(temp);
+        }
+
+        reverse(v.begin(),v.end());
+        for(int i=0;i<n;i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
